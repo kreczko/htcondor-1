afc4f621bb4033d42295b306002d06944cbbfe27
kreczko <lkreczko@googlemail.com>
2017-01-03 16:22:13 +0000

    completing setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,12 @@ HTCONDOR_TMP = 'tmp'
 HTCONDOR_PACKAGES = ['htcondor', 'classad_module']
 PYTHON_LIBRARIES = [os.path.join(HTCONDOR_TMP, 'src', 'python-bindings', l)
                     for l in ['classad.so', 'htcondor.so']]
+LIBRARIES = [
+    os.path.join(HTCONDOR_TMP, 'src', 'python-bindings',
+                 'libpyclassad2.7_*_*_*.so'),
+    os.path.join(HTCONDOR_TMP, 'src', 'condor_utils',
+                 'libcondor_utils_*_*_*.so'),
+]
 
 
 def condor_version():
@@ -21,7 +27,8 @@ def condor_version():
     p = subprocess.Popen([cmd], stdout=subprocess.PIPE)
     output = p.communicate()[0]
     if not p.returncode == 0:
-        raise RuntimeWarning('Something went wrong during call of `condor_version`\n Is htcondor installed?')
+        raise RuntimeWarning(
+            'Something went wrong during call of `condor_version`\n Is htcondor installed?')
     version = output.split(' ')[1]
     return version
 
@@ -60,7 +67,8 @@ def compile_python_bindings():
         'make {PACKAGES}',
     ]
     if platform.dist()[0].lower() == 'ubuntu':
-        commands.insert(1, 'curl http://t2.unl.edu/store/sources/condor-prefix.tar.gz | tar zxv')
+        commands.insert(
+            1, 'curl http://t2.unl.edu/store/sources/condor-prefix.tar.gz | tar zxv')
     all_in_one = ' && '.join(commands)
     all_in_one = all_in_one.format(
         HTCONDOR_TMP=HTCONDOR_TMP,
@@ -68,6 +76,7 @@ def compile_python_bindings():
         PACKAGES=' '.join(HTCONDOR_PACKAGES),
     )
     subprocess.call(all_in_one, shell=True)
+
 
 class HTCondorInstall(DistutilsInstall):
 
@@ -98,10 +107,17 @@ class HTCondorInstall(DistutilsInstall):
         for f in PYTHON_LIBRARIES:
             dst = os.path.join(target_dir, os.path.basename(f))
             shutil.copyfile(f, dst)
+        if platform.dist()[0].lower() == 'ubuntu':
+            target_dir = '/usr/lib'
+        else:
+            target_dir = '/usr/lib64'
+        for f in LIBRARIES:
+            dst = os.path.join(target_dir, os.path.basename(f))
+            shutil.copyfile(f, dst)
 
     def _post_install(self):
         pass
-        #shutil.rmtree(HTCONDOR_TMP)
+        # shutil.rmtree(HTCONDOR_TMP)
 
 setup(
     name='htcondor',
